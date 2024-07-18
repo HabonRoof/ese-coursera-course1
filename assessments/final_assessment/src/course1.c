3510b8fe261f60761dc498ab4549d856140e74ef
HabonRoof <johnson35762@gmail.com>
2024-07-18 08:21:04 +0800

    testing atoi

--- assessments/final_assessment/src/course1.c
+++ assessments/final_assessment/src/course1.c
@@ -40,10 +40,11 @@ int8_t test_data1() {
   }
 
   digits = my_itoa( num, ptr, BASE_16);   
+  PRINTF("  my_itoa digits: %d\n", num);
+  PRINTF("  my_itoa result: %d\n", num);
   value = my_atoi( ptr, digits, BASE_16);
   #ifdef VERBOSE
   PRINTF("  Initial number: %d\n", num);
-  PRINTF("  ascii number: %d\n", digits);
   PRINTF("  Final Decimal number: %d\n", value);
   #endif
   free_words( (uint32_t*)ptr );
@@ -73,7 +74,7 @@ int8_t test_data2() {
   value = my_atoi( ptr, digits, BASE_10);
   #ifdef VERBOSE
   PRINTF("  Initial Decimal number: %d\n", num);
-  PRINTF("  Final Decimal number: %d\n", value);
+  PRINTF("  Final Decimal number: %s\n", ptr);
   #endif
   free_words( (uint32_t*)ptr );
 
